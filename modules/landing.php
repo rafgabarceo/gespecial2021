3066f032dac0fe4e441eeefccb4af22675c33e2c
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-08-21 23:12:07 +0800

    Draft 1 of the landing page. Tweaked some things a bit

---
+++ modules/landing.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <body>
+      <div class="grid grid-cols-2 gap-8 px-4 align-middle">
+        <div class="landing-text">
+            <img src="/images/TLS - Star 1.png" class="thelasallian-star"  alt="tls-star">
+            <h1 class="thelasallian-text thelasallian-animation">The LaSallian</h1>
+            <h3 class="thelasallian-sub thelasallian-animation">General Elections Special 2021</h3>
+        </div>
+        <div class="flex flex-col justify-center">
+            <div>
+                <img class="top" src="images/landing/top.png"/>
+                <img class="handwpaper" src="images/landing/handwpaper.png"/>
+                <img class="box" src="images/landing/fullbox.png"/>
+            </div>
+        </div>
+      </div>
+    </body>
+</html>
+      
