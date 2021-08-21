9701bb70988f6711ef7693e8e1bc64583ec91519
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-08-21 20:16:56 +0800

    Transferred header styling into header.php

--- modules/header.php
+++ modules/header.php
@@ -1,4 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+        <title>General Elections Special 2021</title>
+            <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="/css/css.css">
+    </head>
+<body>
+    <nav class="bg-white-1000 shadow-lg navbar-tls fixed w-full z-10">
+        <div class="mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-center h-16">
+            <div class="flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="hidden sm:block sm:ml-6">
+                <div class="flex space-x-6">
+                <a href="#" class="text-black-300 hover:text-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium aria-current="page">Home</a>
+                <a href="#" class="text-black-300 hover:text-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Head to Head</a>
+                <a href="#" class="text-black-300 hover:text-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Fast Talk</a>
+                <div class="flex items-center">
+                    <img class="block lg:hidden h-8 w-auto" src="images\TLS - Star 1.png" alt="Workflow">
+                    <img class="hidden lg:block h-8 w-auto" src="images\TLS - Star 1.png" alt="Workflow">
+                    </div>
+                <a href="#" class="text-black-300 hover:text-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Articles</a>
+                <a href="#" class="text-black-300 hover:text-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Vox Populi</a>
+                <a href="#" class="text-black-300 hover:text-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Daily Tallies</a>
+                </div>
+            </div>
+            </div>
+        </div> 
+    </div>
+        </nav>
+</body>
 </html>
