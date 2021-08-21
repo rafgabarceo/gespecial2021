09ff598969e78e39d121471cc4d5c2aa0a27dcfa
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-08-21 18:56:35 +0800

    Added scroll function and mobile nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 800) {
-      $('.navbar-tls').fadeIn();
-    } else {
-      $('.navbar-tls').fadeOut();
-    }
-  });
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 1) {
+    $('.navbar-tls').fadeIn();
+  } else {
+    $('.navbar-tls').fadeOut();
+  }
+});
+
+
+const btn = document.querySelector('.mob-button');
+const menu = document.querySelector('.mobile-menu');
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle("hidden");
+});
