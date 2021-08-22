9d98db0622ca1a0bfa3d4947f97938d643efb8e1
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-08-22 21:34:05 +0800

    Moved the location of the header from the center to the left for balance.

--- modules/articles.php
+++ modules/articles.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <body>
-    <h1 class="text-center text-green-700 text-6xl">Articles</h1>
+    <h1 class="pl-10 text-green-700 text-6xl">Articles</h1>
     <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         <?php include_once "modules/articleCard.php"?>
     </div>
