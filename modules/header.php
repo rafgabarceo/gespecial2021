315358b71eca7d2e51add661f92122b4c328941f
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-08-27 16:25:37 +0800

    Added AOS ccs

--- modules/header.php
+++ modules/header.php
@@ -4,6 +4,7 @@
 <head>
     <title>General Elections Special 2021</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="css/css.css">
 </head>
 
