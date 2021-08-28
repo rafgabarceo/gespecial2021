82c95e7dd702da7e717ae544ac4cd9825e317187
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-08-28 21:26:21 +0800

    Added offices module

---
+++ modules/officers.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<body>
+    <h1 class="text-gray-100 text-6xl text-center py-5 font-bold" data-aos="zoom-in">Offices</h1>
+    <div class="grid p-20 pt-0 justify-around items-center gap-5 lg:grid-cols-2 md:grid-cols-1">
+        <div class="card-1 rounded shadow-xl" data-aos="fade-right"><img src="images/offices/of-1.png" alt=""></div>
+        <div class="card-2 rounded shadow-xl" data-aos="fade-left"><img src="images/offices/of-2.png" alt=""></div>
+    </div>
+    <div class="grid p-20 pt-0 justify-center items-center lg:grid-cols-3 md:grid-cols-1">
+        <div class="card-3 rounded shadow-xl" data-aos="fade-left"><img src="images/offices/of-3.png" alt=""></div>
+        <div class="card-3 rounded shadow-xl" data-aos="fade-left"><img src="images/offices/of-4.png" alt=""></div>
+        <div class="card-3 rounded shadow-xl" data-aos="fade-left"><img src="images/offices/of-5.png" alt=""></div>
+    </div>
+</body>
+
+</html>
